4b6f7d7b3a4eb2167639fa138e59f353a8ce7a2f
Jan Stolarek <jan.stolarek@ed.ac.uk>
2017-01-12 12:04:10 +0000

    Silence warnings in Repl

--- src/Repl.hs
+++ src/Repl.hs
@@ -4,14 +4,14 @@ module Repl
     , ParseResult(..)
     ) where
 
-import           Absyn
-import           Desugar        ( desugar     )
+import           Absyn hiding   ( ty             )
+import           Desugar        ( desugar        )
 import           Env
-import           Eval           ( eval        )
+import           Eval           ( eval           )
 import           PrettyPrinting
-import           Resugar
-import qualified Trace as T     ( Value, Type )
-import           UntypedParser  ( parseRepl   )
+import           Resugar        () -- PP instances only
+import qualified Trace as T     ( Value, Type    )
+import           UntypedParser  ( parseRepl      )
 
 import           Control.Monad.State.Strict
 import           Text.ParserCombinators.Parsec ( ParseError )
@@ -23,42 +23,42 @@ data ParseResult = OK
                  | Error ParseError deriving (Eq)
 
 data ReplState = ReplState
-    { tyCtx :: TyCtx       -- ^ data type declarations
-    , gamma :: Env T.Type  -- ^ variable types
-    , env   :: Env T.Value -- ^ variable values
+    { tyCtxSt :: TyCtx       -- ^ data type declarations
+    , gammaSt :: Env T.Type  -- ^ variable types
+    , envSt   :: Env T.Value -- ^ variable values
     }
 
 emptyState :: ReplState
-emptyState = ReplState { tyCtx = emptyTyCtx
-                       , gamma = emptyEnv
-                       , env   = emptyEnv }
+emptyState = ReplState { tyCtxSt = emptyTyCtx
+                       , gammaSt = emptyEnv
+                       , envSt   = emptyEnv }
 
 getTyCtx :: ReplM TyCtx
 getTyCtx = do
-  ReplState { tyCtx = tyCtx } <- get
+  ReplState { tyCtxSt = tyCtx } <- get
   return tyCtx
 
 getEnv :: ReplM (Env T.Value)
 getEnv = do
-  ReplState { env = env } <- get
+  ReplState { envSt = env } <- get
   return env
 
 getGamma :: ReplM (Env T.Type)
 getGamma = do
-  ReplState { gamma = gamma } <- get
+  ReplState { gammaSt = gamma } <- get
   return gamma
 
 addDataDefn :: TyCtx -> ReplM ()
 addDataDefn newCtx = do
   replState <- get
-  put $ replState { tyCtx = newCtx }
+  put $ replState { tyCtxSt = newCtx }
 
 addBinding :: Var -> T.Value -> T.Type -> ReplM ()
 addBinding var val ty = do
   replState <- get
-  let newEnv   = updateEnv (env   replState) var val
-      newGamma = updateEnv (gamma replState) var ty
-  put $ replState { env = newEnv, gamma = newGamma }
+  let newEnv   = updateEnv (envSt   replState) var val
+      newGamma = updateEnv (gammaSt replState) var ty
+  put $ replState { envSt = newEnv, gammaSt = newGamma }
 
 runRepl :: ReplM () -> IO ()
 runRepl repl = evalStateT repl emptyState
@@ -68,17 +68,17 @@ parseAndEvalLine line = do
   tyCtx <- getTyCtx
   case parseRepl line tyCtx of
     Left err -> return (Error err)
-    Right (tyCtx, Nothing ) -> addDataDefn tyCtx >> return OK
-    Right (_    , Just (LetR var exp)) ->
+    Right (tyCtx', Nothing ) -> addDataDefn tyCtx' >> return OK
+    Right (_     , Just (LetR var expr)) ->
         do env   <- getEnv
            gamma <- getGamma
-           let (dexp, ty) = desugar tyCtx gamma exp
-           let val        = eval env dexp
+           let (dexpr, ty) = desugar tyCtx gamma expr
+           let val         = eval env dexpr
            val `seq` addBinding var val ty
-           return (It $ "val it =  " ++ show (pp (tyCtx,val)) ++ " : " ++ show (pp ty))
-    Right (_    , Just exp) ->
+           return (It $ "val it = " ++ show (pp (tyCtx,val)) ++ " : " ++ show (pp ty))
+    Right (_     , Just expr) ->
         do env   <- getEnv
            gamma <- getGamma
-           let (dexp, ty) = desugar tyCtx gamma exp
-           let val        = eval env dexp
-           return (It $ "val it =  " ++ show (pp (tyCtx,val)) ++ " : " ++ show (pp ty))
+           let (dexpr, ty) = desugar tyCtx gamma expr
+           let val         = eval env dexpr
+           return (It $ "val it = " ++ show (pp (tyCtx,val)) ++ " : " ++ show (pp ty))
